978a76ab75eac24174b51f24ac0b65aa93d734bb
Suraj Ghimire <Suraj.Ghimire@bridgenext.com>
2025-03-31 13:16:47 +0530

    The table connection and push operation completed via hardcore

--- controllers/property_controller.js
+++ controllers/property_controller.js
@@ -8,7 +8,12 @@ const prisma = new PrismaClient();
 
 const getAllProperties = asyncHandler(async (req, res) => {
     console.log("getAllProperties called !")
-    res.status(200).send("Property of all users [{...}]")
+    res.status(200).send("Getting Property of all users [{...}]")
+    await prisma.Owner.create({
+        data: {
+            name: "Owner1"
+        }
+    })
 })
 
 const setAllProperties = asyncHandler(async (req, res) => {
@@ -22,4 +27,20 @@ const setAllProperties = asyncHandler(async (req, res) => {
     res.status(200).send("Property of all users [{...}]")
 })
 
-module.exports = { getAllProperties, setAllProperties }
+const setProperty = asyncHandler(async (req, res) => {
+    console.log("setAllProperties called !")
+    const name = 'Suraj Ghimire'
+    const callPrisma = await prisma.property.create({
+        data: {
+            name: "Suraj Ghimire",
+            description: "This is the best property of all time",
+            price: 500000,
+            coordinate: { x: '2039490', y: '738384' },
+            images: [{ name: 'Bathroom', images: ["img1.jpg", "img2.jpg"] }, { name: 'Surrounding', images: ["img3.jpg"] }],
+            ownerId: 1
+        }
+    })
+    res.status(200).send("Property of all users [{...}]")
+})
+
+module.exports = { getAllProperties, setAllProperties, setProperty }
--- router/owner_router/property_router.js
+++ router/owner_router/property_router.js
@@ -2,12 +2,13 @@ const express = require('express');
 const propertyRouter = express.Router();
 
 // imports ------------------
-const { getAllProperties, setAllProperties } = require('../../controllers/property_controller');
+const { getAllProperties, setAllProperties, setProperty } = require('../../controllers/property_controller');
 
 
 propertyRouter.route('/getAllProperties').get(getAllProperties)
 
 propertyRouter.route('/setAllProperties').get(setAllProperties)
 
+propertyRouter.route('/setProperty').get(setProperty)
 
 module.exports = propertyRouter;
